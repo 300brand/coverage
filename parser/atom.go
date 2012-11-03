08982364ad70fa45835ef8fcb7eae37e16b3c935
jake <bigjake2s@gmail.com>
2012-11-02 23:54:35 -0400

    Established basis for parsing Atom feeds

--- parser/atom.go
+++ parser/atom.go
@@ -1,8 +1,26 @@
 package parser
 
+import (
+	"encoding/xml"
+)
+
 type Atom struct {
 }
 
+type atom_feed struct {
+	XMLName xml.Name `xml:"http://www.w3.org/2005/Atom feed"`
+	Title   string   `xml:"title"`
+}
+
+func init() {
+	decoders["Atom"] = Atom{}
+}
+
 func (a Atom) Decode(data []byte) (feed Feed, err error) {
+	v := &atom_feed{}
+	if err = xml.Unmarshal(data, v); err != nil {
+		return
+	}
+	feed.Title = v.Title
 	return
 }
--- parser/atom_test.go
+++ parser/atom_test.go
@@ -1,9 +1,37 @@
 package parser
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
+var atomFeed []byte
+
+func init() {
+	t := &testing.T{}
+	f, err := os.Open("headlines.atom")
+	defer f.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	atomFeed, err = ioutil.ReadAll(f)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInit(t *testing.T) {
+	if _, ok := decoders["Atom"]; !ok {
+		t.Error("Atom decoder not found")
+	}
+}
+
 func TestTitle(t *testing.T) {
-	_ = Atom{}
+	a := decoders["Atom"]
+	f, err := a.Decode(atomFeed)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Title: %s", f.Title)
 }
