4b6aea4025a9f6fe0e8c9a069f3d2cdfd8f14ef3
jake <bigjake2s@gmail.com>
2012-11-13 13:37:08 -0500

    Added Parse() tests

--- parser/testfeed/feed_test.go
+++ parser/testfeed/feed_test.go
@@ -8,10 +8,32 @@ import (
 	"testing"
 )
 
+func TestAtomParse(t *testing.T) {
+	f, err := parser.Parse(Atom, "atom")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(f.Articles) != 50 {
+		t.Errorf("Invalid number of articles: %d", len(f.Articles))
+	}
+}
+
 func TestAtomType(t *testing.T) {
 	if typ, err := parser.Type(Atom); err != nil || typ != "atom" {
 		t.Errorf("Expected atom, got %s", typ)
 		t.Error(err)
+	}
+}
+
+func TestRDFParse(t *testing.T) {
+	f, err := parser.Parse(RDF, "rdf")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(f.Articles) != 90 {
+		t.Errorf("Invalid number of articles: %d", len(f.Articles))
 	}
 }
 
@@ -22,10 +44,27 @@ func TestRDFType(t *testing.T) {
 	}
 }
 
+func TestRSSParse(t *testing.T) {
+	f, err := parser.Parse(RSS, "rss")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(f.Articles) != 10 {
+		t.Errorf("Invalid number of articles: %d", len(f.Articles))
+	}
+}
+
 func TestRSSType(t *testing.T) {
 	if typ, err := parser.Type(RSS); err != nil || typ != "rss" {
 		t.Errorf("Expected rss, got %s", typ)
 		t.Error(err)
+	}
+}
+
+func TestInvalidParse(t *testing.T) {
+	if _, err := parser.Parse(Atom, "rss"); err == nil {
+		t.Error("Expected error when using RSS decoder to parse Atom feed")
 	}
 }
 
