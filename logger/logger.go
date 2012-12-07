7eaec5d73eb364275617261418a559626fb0cb53
jake <bigjake2s@gmail.com>
2012-12-07 10:48:09 -0500

    Only 1 var means no var ( ...

--- logger/logger.go
+++ logger/logger.go
@@ -14,14 +14,12 @@ const (
 	flags = log.LstdFlags | log.Lshortfile
 )
 
-var (
-	loggers = [...]*log.Logger{
-		_DEBUG: log.New(os.Stderr, "DEBUG   ", flags),
-		_WARN:  log.New(os.Stderr, "WARNING ", flags),
-		_FATAL: log.New(os.Stderr, "FATAL   ", flags),
-		_PANIC: log.New(os.Stderr, "PANIC   ", flags),
-	}
-)
+var loggers = [...]*log.Logger{
+	_DEBUG: log.New(os.Stderr, "DEBUG   ", flags),
+	_WARN:  log.New(os.Stderr, "WARNING ", flags),
+	_FATAL: log.New(os.Stderr, "FATAL   ", flags),
+	_PANIC: log.New(os.Stderr, "PANIC   ", flags),
+}
 
 func Debug(v ...interface{}) {
 	loggers[_DEBUG].Output(2, fmt.Sprintln(v...))
