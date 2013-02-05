6022b55451308dda30feed3f21bb3e10514ea976
jake <bigjake2s@gmail.com>
2013-02-05 16:26:20 -0500

    Filled in contents for coverage-cleanurl util

--- bin/util/coverage-cleanurl/main.go
+++ bin/util/coverage-cleanurl/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"git.300brand.com/coverage/cleanurl"
+	"net/url"
+	"os"
+)
+
 func main() {
+	flag.Parse()
+	rawurl := flag.Arg(0)
+
+	parsed, err := url.Parse(rawurl)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	cleaned := cleanurl.Clean(parsed)
+	fmt.Println(cleaned)
 }
