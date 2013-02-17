50ad9ec6b5171c67177c12830af517b9417a80f7
jake <bigjake2s@gmail.com>
2013-02-16 21:55:05 -0500

    Simplified execution in coverage-realurl

--- bin/util/coverage-realurl/main.go
+++ bin/util/coverage-realurl/main.go
@@ -10,16 +10,11 @@ import (
 func main() {
 	flag.Parse()
 
-	if len(flag.Args()) == 0 {
-		fmt.Println("No URL supplied")
-		os.Exit(1)
-	}
-
 	url := flag.Arg(0)
 	r, err := downloader.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(2)
+		os.Exit(1)
 	}
 
 	fmt.Println(r.RealURL)
