760e2ac5945569e31a2de3474994666299e3b0f6
jake <bigjake2s@gmail.com>
2013-02-21 17:21:42 -0500

    Slight filter modifications
    * Singularized filter func names
    * Added a little documentation
    * Added BlockElement filter
    * Added NormalizeBlock filter to silently turn block elements into divs

--- page/filter/filter.go
+++ page/filter/filter.go
@@ -5,6 +5,8 @@ import (
 	"code.google.com/p/go.net/html/atom"
 )
 
+// Filter funcs return true when the *html.Node passed in matches some
+// condition.
 type Filter func(n *html.Node) bool
 
 func AnyValid(n *html.Node, filters []Filter) bool {
@@ -16,7 +18,25 @@ func AnyValid(n *html.Node, filters []Filter) bool {
 	return false
 }
 
-func Comments(n *html.Node) bool {
+func BlockElement(n *html.Node) bool {
+	if n.Type != html.ElementNode {
+		return false
+	}
+	switch n.DataAtom {
+	case atom.Article:
+	case atom.Div:
+	case atom.Footer:
+	case atom.Header:
+	case atom.Nav:
+	case atom.Section:
+	case atom.Td:
+	default:
+		return false
+	}
+	return true
+}
+
+func Comment(n *html.Node) bool {
 	return n.Type == html.CommentNode
 }
 
@@ -28,7 +48,17 @@ func Head(n *html.Node) bool {
 	return n.Type == html.ElementNode && n.DataAtom == atom.Head
 }
 
-func Scripts(n *html.Node) bool {
+// Note this always returns false. This filter silently modifies the incoming
+// *html.Node to change the tag to a div for future processing.
+func NormalizeBlock(n *html.Node) bool {
+	if BlockElement(n) {
+		n.DataAtom = atom.Lookup([]byte("div"))
+		n.Data = "div"
+	}
+	return false
+}
+
+func Script(n *html.Node) bool {
 	if n.Type != html.ElementNode {
 		return false
 	}
