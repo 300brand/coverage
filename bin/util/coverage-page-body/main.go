21924771bf6a1147880457cb1edc4ec97521b3d7
jake <bigjake2s@gmail.com>
2013-02-22 16:32:13 -0500

    Split out the cleaning and body snatching into separate funcs

--- bin/util/coverage-page-body/main.go
+++ bin/util/coverage-page-body/main.go
@@ -2,23 +2,42 @@ package main
 
 import (
 	//"code.google.com/p/go.net/html"
+	"flag"
 	"fmt"
 	"git.300brand.com/coverage/page/body"
 	"io/ioutil"
 	"os"
 )
 
+var (
+	cleanedOnly bool
+)
+
+func init() {
+	flag.BoolVar(&cleanedOnly, "clean", false, "Only show the cleaned HTML used before extracing body content")
+}
+
 func main() {
+	flag.Parse()
+
+	var out []byte
+
 	in, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	b, err := body.GetBody(in)
+
+	if cleanedOnly {
+		out, err = body.CleanHTML(in)
+	} else {
+		out, err = body.GetBody(in)
+	}
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
-	fmt.Println("Body:")
-	fmt.Println(b)
+
+	fmt.Printf("%s\n", out)
 }
--- page/body/extract.go
+++ page/body/extract.go
@@ -4,8 +4,7 @@ import (
 	"bytes"
 	"code.google.com/p/go.net/html"
 	"git.300brand.com/coverage/page/filter"
-	"os"
-	//"github.com/moovweb/gokogiri"
+	"github.com/moovweb/gokogiri"
 )
 
 var cleanFilters = filter.Filters{
@@ -16,30 +15,36 @@ var cleanFilters = filter.Filters{
 	filter.NormalizeBlock,
 }
 
-func CleanDOM(n *html.Node) {
-	for c := n.FirstChild; c != nil; {
-		next := c.NextSibling
-		if cleanFilters.Any(c) {
-			n.RemoveChild(c)
-		} else {
-			CleanDOM(c)
-		}
-		c = next
-	}
-}
-
-func GetBody(b []byte) (body string, err error) {
+func CleanHTML(b []byte) (cleaned []byte, err error) {
 	r := bytes.NewReader(b)
 	doc, err := html.Parse(r)
 	if err != nil {
 		return
 	}
-	CleanDOM(doc)
-	html.Render(os.Stdout, doc)
-	//
-	//, err := gokogiri.ParseHtml(b)
-	//if err != nil {
-	//	return
-	//}
+	cleanDOM(doc)
+	buf := &bytes.Buffer{}
+	html.Render(buf, doc)
+	cleaned = buf.Bytes()
 	return
+}
+
+func GetBody(b []byte) (body []byte, err error) {
+	doc, err := gokogiri.ParseHtml(b)
+	if err != nil {
+		return
+	}
+	doc.Root()
+	return
+}
+
+func cleanDOM(n *html.Node) {
+	for c := n.FirstChild; c != nil; {
+		next := c.NextSibling
+		if cleanFilters.Any(c) {
+			n.RemoveChild(c)
+		} else {
+			cleanDOM(c)
+		}
+		c = next
+	}
 }
