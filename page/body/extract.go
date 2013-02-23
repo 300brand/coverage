2863cf2b9ec220668d732a0eac29f0e05028419e
jake <bigjake2s@gmail.com>
2013-02-22 20:20:23 -0500

    Started adding div extractors

--- page/body/extract.go
+++ page/body/extract.go
@@ -1,14 +1,39 @@
 package body
 
 import (
+	"fmt"
 	"github.com/moovweb/gokogiri"
+	"github.com/moovweb/gokogiri/xml"
 )
+
+type Div string
+type Divs []Div
 
 func GetBody(b []byte) (body []byte, err error) {
 	doc, err := gokogiri.ParseHtml(b)
 	if err != nil {
 		return
 	}
-	doc.Root()
+	html := doc.Root().FirstChild()
+	divs, err := GetPDivs(html)
+	if err != nil {
+		return
+	}
+	body = []byte(fmt.Sprintf("%d", len(divs)))
 	return
 }
+
+// $divs = $xpath->query('//div[p]');
+func GetPDivs(n xml.Node) (divs Divs, err error) {
+	results, err := n.Search("//div[p]")
+	if err != nil {
+		return
+	}
+	fmt.Printf("%d\n", len(results))
+	divs = make(Divs, len(results))
+	return
+}
+
+// $divs = $xpath->query('//div[br/following-sibling::*[1][self::br]]');
+//func GetBrBrDivs(doc *html.HtmlDocument) (divs Divs) {
+//}
