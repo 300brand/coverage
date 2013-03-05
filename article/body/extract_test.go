0134dc4283d78307754d289ff9cdbc392fee028f
jake <bigjake2s@gmail.com>
2013-03-05 12:35:28 -0500

    Adding empty tests for bad articles Victor found

--- article/body/extract_test.go
+++ article/body/extract_test.go
@@ -3,3 +3,21 @@ package body
 import (
 	"testing"
 )
+
+// http://www.biosciencetechnology.com/news/2013/03/brain-adds-specified-cells-during-puberty
+// <div>Content</div><div>&nbsp;</div>
+func TestBioscienceTechnology(t *testing.T) {
+
+}
+
+// http://blog.cppionline.org/2012/10/supercharging-us-economy-where-both.html
+// <div>Content</div><div><br></div>
+func TestCPPIOnline(t *testing.T) {
+
+}
+
+// http://blogs.cio.com/security/17837/scientists-create-telepathic-rats-and-robosparrows
+// Missing titles from article list at end of article - need to add \n after ul's li's
+func TestCIOBlogs(t *testing.T) {
+
+}
