e43a9b66c1fb8c552789f550b678c3018209091e
jake <bigjake2s@gmail.com>
2013-03-07 12:21:53 -0500

    Updated coverage-article to use downloader service

--- bin/util/coverage-article/main.go
+++ bin/util/coverage-article/main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"git.300brand.com/coverage"
 	"git.300brand.com/coverage/downloader"
-	"io/ioutil"
-	"net/http"
 	"os"
 )
 
@@ -20,14 +18,22 @@ func init() {
 func main() {
 	flag.Parse()
 
-	r, err := getResponse(flag.Arg(0))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	a := coverage.NewArticle()
+
+	url := flag.Arg(0)
+	if isFile {
+		if url[0] != '/' {
+			wd, err := os.Getwd()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			url = fmt.Sprintf("%s%s%s", wd, string(os.PathSeparator), url)
+		}
+		url = "file://" + url
 	}
 
-	a, err := coverage.NewArticle(&r)
-	if err != nil {
+	if err := downloader.NewService(url).Update(a); err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
@@ -38,28 +44,4 @@ func main() {
 		os.Exit(3)
 	}
 	fmt.Printf("%s\n", out)
-}
-
-func getResponse(path string) (r downloader.Response, err error) {
-	if isFile {
-		pwd, err := os.Getwd()
-		if err != nil {
-			return r, err
-		}
-		f, err := os.Open(path)
-		if err != nil {
-			return r, err
-		}
-		defer f.Close()
-		if r.Body, err = ioutil.ReadAll(f); err != nil {
-			return r, err
-		}
-		pathURL := fmt.Sprintf("file://%s/%s", pwd, path)
-		r.Code = http.StatusOK
-		r.OriginalURL = pathURL
-		r.RealURL = pathURL
-	} else {
-		r, err = downloader.Fetch(path)
-	}
-	return
 }
