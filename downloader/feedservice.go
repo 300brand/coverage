c5ff4daf3d1bc89e2b702d455847d6b2f3a89ff1
jake <bigjake2s@gmail.com>
2013-03-12 09:32:42 -0400

    Added FeedService and PublicationService to downloader

---
+++ downloader/feedservice.go
@@ -0,0 +1,27 @@
+package downloader
+
+import (
+	"git.300brand.com/coverage"
+	"git.300brand.com/coverage/service"
+	"net/url"
+	"time"
+)
+
+type FeedService struct {
+}
+
+var _ service.FeedService = FeedService{}
+
+func NewFeedService() FeedService {
+	return FeedService{}
+}
+
+func (s FeedService) Update(f *coverage.Feed) error {
+	f.Log.Service("downloader.FeedService")
+	r, err := Fetch(f.URL.String())
+	if err != nil {
+		return f.Log.Error(err)
+	}
+	f.Downloaded()
+	return nil
+}
