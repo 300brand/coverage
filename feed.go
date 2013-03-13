ed14bd4a0a351232b057380f07a8f70750e475ef
jake <bigjake2s@gmail.com>
2013-03-13 12:50:46 -0400

    Finished up the parser FeedService

--- feed.go
+++ feed.go
@@ -11,7 +11,8 @@ type Feed struct {
 	ID           bson.ObjectId `bson:"_id"`
 	URL          *url.URL
 	Content      []byte     `bson:"-"`
-	Articles     []*Article `bson:"-"`
+	Articles     []*Article `bson:"-"` // Temporary Article storage
+	URLs         []*url.URL
 	Log          logger.Entries
 	Added        time.Time
 	Updated      time.Time
@@ -25,6 +26,17 @@ func NewFeed() (f *Feed) {
 	}
 	f.Log.Debug("Created: %s", f.ID.Hex())
 	return
+}
+
+func (f *Feed) AddURL(u *url.URL) bool {
+	s := u.String()
+	for _, v := range f.URLs {
+		if v.String() == s {
+			return false
+		}
+	}
+	f.URLs = append(f.URLs, u)
+	return true
 }
 
 func (f *Feed) Files() []File {
--- parser/feedservice.go
+++ parser/feedservice.go
@@ -14,17 +14,29 @@ type FeedService struct{}
 
 var _ service.FeedService = &FeedService{}
 
+func NewFeedService() *FeedService {
+	return &FeedService{}
+}
+
 func (s *FeedService) Update(f *coverage.Feed) error {
+	f.Log.Service("parser.FeedService")
+
 	n := &normalizer.Default{}
 	if err := Normalize(f.Content, n); err != nil {
 		return f.Log.Error(err)
 	}
 	for _, article := range n.Articles {
+		if !f.AddURL(article.URL) {
+			continue
+		}
+
 		a := coverage.NewArticle()
+		a.FeedId = f.ID
 		a.Title = article.Title
-		a.URL = &article.URL
+		a.URL = article.URL
 		a.Published = article.Published
 		f.Articles = append(f.Articles, a)
+		f.Log.Debug("New article: %s", a.ID.Hex())
 	}
 	return nil
 }
