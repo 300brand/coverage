6d0b39ba2035583cf52c0e3e9856ce1831027d45
jake <bigjake2s@gmail.com>
2013-03-13 12:54:44 -0400

    Added utility script to process feeds and articles

---
+++ bin/util/coverage-feed-articles/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"git.300brand.com/coverage/article/body"
+	"git.300brand.com/coverage/downloader"
+	"git.300brand.com/coverage/logger"
+	"git.300brand.com/coverage/parser"
+	"git.300brand.com/coverage/storage/mongo"
+	"labix.org/v2/mgo/bson"
+	"log"
+	"os"
+)
+
+var config = struct {
+	DBHost  string
+	DBName  string
+	ID      bson.ObjectId
+	NewOnly bool
+	Verbose bool
+}{}
+
+func init() {
+	flag.BoolVar(&config.NewOnly, "new", false, "Only show new articles")
+	flag.BoolVar(&config.Verbose, "v", false, "Verbose")
+	flag.StringVar(&config.DBHost, "dbhost", "localhost", "Mongo server")
+	flag.StringVar(&config.DBName, "dbname", "Coverage", "Mongo database name")
+}
+
+func main() {
+	flag.Parse()
+
+	if config.Verbose {
+		logger.EnableLogging(os.Stdout)
+	}
+
+	if !bson.IsObjectIdHex(flag.Arg(0)) {
+		log.Fatalf("Invalid bson.ObjectId: %s", flag.Arg(0))
+	}
+	config.ID = bson.ObjectIdHex(flag.Arg(0))
+
+	m := mongo.New(config.DBHost, config.DBName)
+	if err := m.Connect(); err != nil {
+		log.Fatal(err)
+	}
+	defer m.Close()
+
+	feed, err := m.GetFeed(config.ID)
+	if err != nil {
+		log.Fatalf("Could not find feed with ID: %s", config.ID.Hex())
+	}
+
+	log.Println("Downloading new feed")
+	downloader.NewFeedService().Update(feed)
+
+	log.Println("Saving data")
+	mfs := mongo.NewFeedService(m)
+	mfs.Update(feed)
+
+	log.Println("Building article list")
+	parser.NewFeedService().Update(feed)
+
+	das := downloader.NewArticleService()
+	bas := body.NewArticleService()
+	mas := mongo.NewArticleService(m)
+	for _, a := range feed.Articles {
+		log.Printf("Processing %s", a.URL)
+		das.Update(a)
+		bas.Update(a)
+		if err := mas.Update(a); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Println("Updating feed")
+	mfs.Update(feed)
+}
