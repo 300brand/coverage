e89e4c2a5b46cb767ba3fb75ff0270c253b1cf9d
jake <bigjake2s@gmail.com>
2013-03-15 08:56:21 -0400

    Added service test to ensure the interface is respected

---
+++ service/service_test.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"git.300brand.com/coverage"
+	"testing"
+)
+
+type as struct{}
+type fs struct{}
+type ps struct{}
+
+func (s as) Update(a *coverage.Article) error     { return nil }
+func (s fs) Update(f *coverage.Feed) error        { return nil }
+func (s ps) Update(p *coverage.Publication) error { return nil }
+
+func TestArticleService(t *testing.T)     { var _ ArticleService = as{} }
+func TestFeedService(t *testing.T)        { var _ FeedService = fs{} }
+func TestPublicationService(t *testing.T) { var _ PublicationService = ps{} }
