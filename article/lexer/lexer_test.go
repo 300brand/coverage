c370664b4b25d12c60451089ac6b89be8ac11fa3
jake <bigjake2s@gmail.com>
2013-03-18 17:16:13 -0400

    Added Stemming benchmark; finished TestWordsLen

--- article/lexer/lexer_test.go
+++ article/lexer/lexer_test.go
@@ -12,16 +12,22 @@ func BenchmarkGetWords(b *testing.B) {
 	}
 }
 
-func TestWordStemIndex(t *testing.T) {
-	/*
-		s := []byte(`
-			As open government gains favor and reaps benefits for the federal
-			workforce and citizens, Data.gov continues to build a Google-style
-			universe based on the concept and expand its mission globally.
-		`)
-		for _, w := range GetWords(s) {
-			t.Logf("%2d %-14s %s", w.Index, w.Word, w.Stem)
-		}
-	*/
-	t.Skip("TODO")
+func BenchmarkStemming(b *testing.B) {
+	StemmingEnabled = true
+	for i := 0; i < b.N; i++ {
+		GetWords(benchmarkString)
+	}
+	StemmingEnabled = false
+}
+
+func TestWordsLen(t *testing.T) {
+	s := []byte(`
+		As open government gains favor and reaps benefits for the federal
+		workforce and citizens, Data.gov continues to build a Google-style
+		universe based on the concept and expand its mission globally.
+	`)
+	words := GetWords(s)
+	if l := len(words); l != 31 {
+		t.Errorf("Got %d words", l)
+	}
 }
