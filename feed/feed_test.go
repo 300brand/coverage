46e109e89196cf4fffb80f89880a09ea5a8c898b
jake <bigjake2s@gmail.com>
2013-03-19 11:56:33 -0400

    Updated feed_test to use the samples for testing

--- feed/feed_test.go
+++ feed/feed_test.go
@@ -1,76 +1,91 @@
 package feed
 
 import (
-	//"git.300brand.com/coverage/feed/normalizer"
-	//"git.300brand.com/coverage/feed/parser"
+	"git.300brand.com/coverage"
+	"git.300brand.com/coverage/feed/parser"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	_ "git.300brand.com/coverage/feed/parser/atom"
 	_ "git.300brand.com/coverage/feed/parser/rdf"
 	_ "git.300brand.com/coverage/feed/parser/rss"
-	"log"
-	"path/filepath"
 )
 
 type test struct {
+	Type  string
+	Feed  *coverage.Feed
 	Files struct {
-		In   []string
-		URLs []string
+		URLs string
 	}
-	Type string
-	Len  int
 }
 
-var tests = []test{}
+var (
+	feedTypes = []string{"atom", "rdf", "rss"}
+	tests     = []test{}
+)
 
 func init() {
-	for _, ext := range []string{"atom", "rdf", "rss"} {
+	for _, ext := range feedTypes {
 		list, err := filepath.Glob("samples/*." + ext)
 		if err != nil {
 			log.Fatal(err)
 		}
 		for _, filename := range list {
-			log.Println(filename)
+			t := test{}
+			t.Files.URLs = strings.Replace(filename, "."+ext, ".urls", 1)
+			t.Type = ext
+
+			t.Feed = coverage.NewFeed()
+			if t.Feed.Content, err = ioutil.ReadFile(filename); err != nil {
+				log.Fatal(err)
+			}
+
+			s := NewFeedService()
+			if err = s.Update(t.Feed); err != nil {
+				log.Fatal(err)
+			}
+			tests = append(tests, t)
 		}
 	}
 }
 
-func TestURLs(t *testing.T) {}
-
-/*
-func TestParse(t *testing.T) {
+func TestURLs(t *testing.T) {
 	for _, test := range tests {
-		n := &normalizer.Default{}
-		if err := parser.Normalize(test.Data, n); err != nil {
-			t.Error(err)
+		if _, err := os.Stat(test.Files.URLs); err != nil {
+			t.Logf("Skipping; no %s file found", test.Files.URLs)
 			continue
 		}
-		if len(n.Articles) != test.Len {
-			t.Errorf("Invalid number of articles: %d", len(n.Articles))
+
+		b, err := ioutil.ReadFile(test.Files.URLs)
+		if err != nil {
+			t.Errorf("Read error: %s", err)
+		}
+
+		urls := strings.Fields(string(b))
+		if l := len(test.Feed.Articles); l != len(urls) {
+			t.Errorf("Invalid URL count\nExpect: %d\nGot: %d", len(urls), l)
+		}
+
+		for i, u := range urls {
+			if aURL := test.Feed.Articles[i].URL; u != aURL.String() {
+				t.Errorf("URL Mismatch\nExpect: %s\nGot: %s", u, aURL)
+			}
 		}
 	}
 }
 
 func TestType(t *testing.T) {
 	for _, test := range tests {
-		if typ, err := parser.Type(test.Data); err != nil || typ != test.Type {
-			t.Errorf("Expected %s, got %s", test.Type, typ)
-			t.Error(err)
+		typ, err := parser.Type(test.Feed.Content)
+		if err != nil {
+			t.Errorf("Decoder error: %s", err)
+		}
+		if typ != test.Type {
+			t.Errorf("Invalid type\nExpect: %s\nGot: %s", test.Type, typ)
 		}
 	}
 }
-
-func TestInvalidParse(t *testing.T) {
-	if _, err := parser.ParseType(Atom, "rss"); err == nil {
-		t.Error("Expected error when using RSS decoder to parse Atom feed")
-	}
-}
-
-func TestInvalidType(t *testing.T) {
-	data := []byte(`<?xml version="1.0"?><bunk><feed /></bunk>`)
-	if typ, err := parser.Type(data); err == nil || typ != "" {
-		t.Errorf("Expected blank type and error, got %s", typ)
-		t.Error(err)
-	}
-}
-*/
