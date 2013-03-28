b14507858dfe5f491a0363d6a1805c9645d6644a
jake <bigjake2s@gmail.com>
2013-03-28 17:47:26 -0400

    Changed signature of GetWords to return []string

--- article/lexer/lexer.go
+++ article/lexer/lexer.go
@@ -1,25 +1,13 @@
 package lexer
 
 import (
-	"bytes"
 	"git.300brand.com/coverage"
 	"github.com/rookii/paicehusk"
+	"strings"
 )
 
 var StemmingEnabled = false
 
-func GetWords(b []byte) (ws coverage.Words) {
-	n := Normalize(b)
-	fields := bytes.Fields(n)
-	for _, f := range fields {
-		w := coverage.Word{
-			Word: string(f),
-		}
-		// Stemming adds 10x the time to split words up
-		if StemmingEnabled {
-			w.Stem = paicehusk.DefaultRules.Stem(w.Word)
-		}
-		ws.Add(w)
-	}
-	return
+func GetWords(b []byte) []string {
+	return strings.Fields(string(Normalize(b)))
 }
