a1a610aa57ddb78f81d7daca5c11542e24db7fe4
jake <bigjake2s@gmail.com>
2013-04-09 11:29:55 -0400

    Completed push/pull for queue state in mongo

--- storage/mongo/queue.go
+++ storage/mongo/queue.go
@@ -2,12 +2,36 @@ package mongo
 
 import (
 	"git.300brand.com/coverage/bridge"
+	"time"
 )
 
-func (m *Mongo) QueueLastId() (id int, err error) {
-	return
+type queueState struct {
+	LastId  uint64
+	LastRun time.Time
+}
+
+const QueueStateCollection = "QueueState"
+
+func (m *Mongo) QueueLastId() (id uint64, err error) {
+	s := &queueState{}
+	q := m.db.C(QueueStateCollection).Find(nil)
+
+	n, err := q.Count()
+	if err != nil {
+		return
+	}
+
+	// new/empty collection
+	if n == 0 {
+		return 0, nil
+	}
+
+	err = q.One(s)
+	return s.LastId, err
 }
 
 func (m *Mongo) UpdateQueue(q bridge.Queue) (err error) {
+	state := &queueState{q.LastId, time.Now()}
+	_, err = m.db.C(QueueStateCollection).Upsert(nil, state)
 	return
 }
