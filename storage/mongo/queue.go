d65622140fc25d7e6ee111784942f815aa46aeff
jake <bigjake2s@gmail.com>
2013-04-09 08:11:49 -0400

    Empty funcs to store and fetch the queue

---
+++ storage/mongo/queue.go
@@ -0,0 +1,13 @@
+package mongo
+
+import (
+	"git.300brand.com/coverage/bridge"
+)
+
+func (m *Mongo) QueueLastId() (id int, err error) {
+	return
+}
+
+func (m *Mongo) UpdateQueue(q bridge.Queue) (err error) {
+	return
+}
