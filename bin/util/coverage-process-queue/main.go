6d01fa4a2e4e0aaef8b1c89417ee72f6ab3ecab6
jake <bigjake2s@gmail.com>
2013-04-09 11:27:55 -0400

    Better error messages

--- bin/util/coverage-process-queue/main.go
+++ bin/util/coverage-process-queue/main.go
@@ -9,21 +9,23 @@ import (
 func main() {
 	m := mongo.New("localhost", "CoverageQueue")
 	if err := m.Connect(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("m.Connect: %s", err)
 	}
 	defer m.Close()
 
 	lastId, err := m.QueueLastId()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("m.QueueLastId: %s", err)
 	}
 
 	queue, err := bridge.GetQueue(lastId, 1)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("bridge.GetQueue: %s", err)
 	}
 
 	log.Printf("%+v", queue)
 
-	m.UpdateQueue(queue)
+	if err := m.UpdateQueue(queue); err != nil {
+		log.Fatalf("m.UpdateQueue: %s", err)
+	}
 }
