62e15126bc80cebb56d4d5b0005065e84dc840b7
jake <bigjake2s@gmail.com>
2013-04-09 15:27:28 -0400

    Finished up coverage-process-queue flow

--- bin/util/coverage-process-queue/main.go
+++ bin/util/coverage-process-queue/main.go
@@ -7,15 +7,21 @@ import (
 )
 
 func main() {
-	m := mongo.New("localhost", "CoverageQueue")
+	q := mongo.New("localhost", "CoverageQueue")
+	if err := q.Connect(); err != nil {
+		log.Fatalf("q.Connect: %s", err)
+	}
+	defer q.Close()
+
+	m := mongo.New("localhost", "Coverage")
 	if err := m.Connect(); err != nil {
 		log.Fatalf("m.Connect: %s", err)
 	}
 	defer m.Close()
 
-	lastId, err := m.QueueLastId()
+	lastId, err := q.QueueLastId()
 	if err != nil {
-		log.Fatalf("m.QueueLastId: %s", err)
+		log.Fatalf("q.QueueLastId: %s", err)
 	}
 
 	queue, err := bridge.GetQueue(lastId, 1)
@@ -23,9 +29,15 @@ func main() {
 		log.Fatalf("bridge.GetQueue: %s", err)
 	}
 
-	log.Printf("%+v", queue)
+	rs := mongo.NewReportService(m)
+	for _, r := range queue.Reports {
+		log.Printf("Adding Report: %s", r.ID.Hex())
+		if err := rs.Update(&r); err != nil {
+			log.Fatalf("rs.Update: %s", err)
+		}
+	}
 
-	if err := m.UpdateQueue(queue); err != nil {
-		log.Fatalf("m.UpdateQueue: %s", err)
+	if err := q.UpdateQueue(queue); err != nil {
+		log.Fatalf("q.UpdateQueue: %s", err)
 	}
 }
