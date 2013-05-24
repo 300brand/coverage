f738bf8b80c172734df96311b483d4a79c7098bb
jake <bigjake2s@gmail.com>
2013-05-24 11:50:46 -0400

    Moved listening address to a flag

--- bin/server-jsonrpc/main.go
+++ bin/server-jsonrpc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
@@ -9,14 +10,19 @@ import (
 	"os"
 )
 
-var s = rpc.NewServer()
+var (
+	s      = rpc.NewServer()
+	listen = flag.String("l", ":8080", "Address to listen on")
+)
 
 func init() {
 	s.RegisterCodec(json.NewCodec(), "application/json")
 }
 
 func main() {
+	flag.Parse()
+
 	log.Print("Starting...")
 	http.Handle("/rpc", handlers.LoggingHandler(os.Stdout, s))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(*listen, nil))
 }
