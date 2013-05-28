bedfe78c28f88d5760dfc899930c7506a0d0319b
jake <bigjake2s@gmail.com>
2013-05-28 14:06:21 -0400

    Removed mongo connections in favor of StorageReader/Writer service

--- bin/service-Publication/service.go
+++ bin/service-Publication/service.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"errors"
-	"git.300brand.com/coverage"
-	"git.300brand.com/coverage/storage/mongo"
 	"github.com/skynetservices/skynet"
+	"github.com/skynetservices/skynet/client"
 	"github.com/skynetservices/skynet/service"
-	"strings"
 )
 
 type Publication struct {
-	Log       skynet.SemanticLogger
-	MongoHost string
-	MongoDb   string
-	m         *mongo.Mongo
+	Log    skynet.SemanticLogger
+	Reader *client.ServiceClient
+	Writer *client.ServiceClient
 }
 
 var _ service.ServiceDelegate = &Publication{}
@@ -28,22 +24,12 @@ func (s *Publication) Unregistered(service *service.Service) {
 
 func (s *Publication) Started(service *service.Service) {
 	s.Log.Trace("Started")
-	s.m = mongo.New(s.MongoHost, s.MongoDb)
-	s.Log.Trace("Connecting to MongoDB: " + s.MongoHost + " " + s.MongoDb)
-	if err := s.m.Connect(); err != nil {
-		s.Log.Fatal(err.Error())
-	}
-	s.Log.Trace("Connected to MongoDB")
-	if err := s.m.EnsureIndexes(); err != nil {
-		s.Log.Fatal(err.Error())
-	}
-	s.Log.Trace("Ensured indexes")
+	s.Reader = c.GetService("StorageReader", "", "", "")
+	s.Writer = c.GetService("StorageWriter", "", "", "")
 }
 
 func (s *Publication) Stopped(service *service.Service) {
 	s.Log.Trace("Stopped")
-	s.m.Close()
-	s.Log.Trace("Closed connection to MongoDB")
 }
 
 func (s *Publication) MethodCalled(method string) {
@@ -53,5 +39,3 @@ func (s *Publication) MethodCalled(method string) {
 func (s *Publication) MethodCompleted(method string, duration int64, err error) {
 	s.Log.Trace("MethodCompleted")
 }
-
-
