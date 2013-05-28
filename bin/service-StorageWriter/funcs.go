8d66760a0393d03a5a4b889203bc4eedd03906b3
jake <bigjake2s@gmail.com>
2013-05-28 11:52:19 -0400

    Filled in main funcs for StorageWriter service

--- bin/service-StorageWriter/funcs.go
+++ bin/service-StorageWriter/funcs.go
@@ -5,14 +5,14 @@ import (
 	"github.com/skynetservices/skynet"
 )
 
-func (s *StorageWriter) UpdateArticle(ri *skynet.RequestInfo, in *coverage.Article, out *coverage.Article) (err error) {
-	return
+func (s *StorageWriter) UpdateArticle(ri *skynet.RequestInfo, a *coverage.Article, out *coverage.Article) (err error) {
+	return s.Mongo.UpdateArticle(a)
 }
 
-func (s *StorageWriter) UpdateFeed(ri *skynet.RequestInfo, in *coverage.Feed, out *coverage.Feed) (err error) {
-	return
+func (s *StorageWriter) UpdateFeed(ri *skynet.RequestInfo, f *coverage.Feed, out *coverage.Feed) (err error) {
+	return s.Mongo.UpdateFeed(f)
 }
 
-func (s *StorageWriter) UpdatePublication(ri *skynet.RequestInfo, in *coverage.Publication, out *coverage.Publication) (err error) {
-	return
+func (s *StorageWriter) UpdatePublication(ri *skynet.RequestInfo, p *coverage.Publication, out *coverage.Publication) (err error) {
+	return s.Mongo.UpdatePublication(p)
 }
