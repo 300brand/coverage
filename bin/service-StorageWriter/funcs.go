e36a605bf891e4254dedfcdfdedf5f453ea124f4
jake <bigjake2s@gmail.com>
2013-05-27 21:28:08 -0400

    Added StorageWriter service

---
+++ bin/service-StorageWriter/funcs.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"git.300brand.com/coverage"
+	"github.com/skynetservices/skynet"
+)
+
+func (s *StorageWriter) UpdateArticle(ri *skynet.RequestInfo, in *coverage.Article, out *coverage.Article) (err error) {
+	return
+}
+
+func (s *StorageWriter) UpdateFeed(ri *skynet.RequestInfo, in *coverage.Feed, out *coverage.Feed) (err error) {
+	return
+}
+
+func (s *StorageWriter) UpdatePublication(ri *skynet.RequestInfo, in *coverage.Publication, out *coverage.Publication) (err error) {
+	return
+}
--- bin/service-StorageWriter/service.go
+++ bin/service-StorageWriter/service.go
@@ -7,17 +7,46 @@ import (
 )
 
 type StorageWriter struct {
-	Log   skynet.SemanticLogger
-	Mongo *mongo.Mongo
+	Log       skynet.SemanticLogger
+	Mongo     *mongo.Mongo
+	MongoHost string
+	MongoDb   string
 }
 
 var _ service.ServiceDelegate = &StorageWriter{}
 
-func (s *StorageWriter) Registered(service *service.Service)   { s.Log.Trace("Registered") }
-func (s *StorageWriter) Unregistered(service *service.Service) { s.Log.Trace("Unregistered") }
-func (s *StorageWriter) Started(service *service.Service)      { s.Log.Trace("Started") }
-func (s *StorageWriter) Stopped(service *service.Service)      { s.Log.Trace("Stopped") }
-func (s *StorageWriter) MethodCalled(method string)            { s.Log.Trace("MethodCalled") }
+func (s *StorageWriter) Registered(service *service.Service) {
+	s.Log.Trace("Registered")
+}
+
+func (s *StorageWriter) Unregistered(service *service.Service) {
+	s.Log.Trace("Unregistered")
+}
+
+func (s *StorageWriter) Started(service *service.Service) {
+	s.Log.Trace("Started")
+	s.Mongo = mongo.New(s.MongoHost, s.MongoDb)
+	s.Log.Trace("Connecting to MongoDB: " + s.MongoHost + " " + s.MongoDb)
+	if err := s.Mongo.Connect(); err != nil {
+		s.Log.Fatal(err.Error())
+	}
+	s.Log.Trace("Connected to MongoDB")
+	if err := s.Mongo.EnsureIndexes(); err != nil {
+		s.Log.Fatal(err.Error())
+	}
+	s.Log.Trace("Ensured indexes")
+}
+
+func (s *StorageWriter) Stopped(service *service.Service) {
+	s.Log.Trace("Stopped")
+	s.Mongo.Close()
+	s.Log.Trace("Closed connection to MongoDB")
+}
+
+func (s *StorageWriter) MethodCalled(method string) {
+	s.Log.Trace("MethodCalled")
+}
+
 func (s *StorageWriter) MethodCompleted(method string, duration int64, err error) {
 	s.Log.Trace("MethodCompleted")
 }
