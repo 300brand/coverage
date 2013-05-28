9f31265cc7796420f016e9e5f8a4ab7373ea47cd
jake <bigjake2s@gmail.com>
2013-05-28 16:14:21 -0400

    Set up StorageReader service

--- bin/service-StorageReader/main.go
+++ bin/service-StorageReader/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"github.com/skynetservices/skynet"
+	"github.com/skynetservices/skynet/service"
+	"os"
+)
+
+var (
+	mongoDb   = skynet.GetDefaultEnvVar("SERVICE_MONGO_DB", "Coverage")
+	mongoHost = skynet.GetDefaultEnvVar("SERVICE_MONGO_HOST", "localhost")
+)
+
+func main() {
+	config, _ := skynet.GetServiceConfig()
+	config.Name = "StorageReader"
+	config.Version = "1"
+
+	s := &StorageReader{
+		Log:       skynet.NewConsoleSemanticLogger(config.Name, os.Stdout),
+		MongoHost: mongoHost,
+		MongoDb:   mongoDb,
+	}
+
+	service := service.CreateService(s, config)
+	defer service.Shutdown()
+
+	waiter := service.Start(true)
+	waiter.Wait()
+}
