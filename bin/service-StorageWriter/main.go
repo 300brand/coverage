e82027351c2b12f25c084a9655e79862d12b6e1e
jake <bigjake2s@gmail.com>
2013-05-29 18:02:06 -0400

    Filled in desired API for StorageWriter; added link to StorageReader

--- bin/service-StorageWriter/main.go
+++ bin/service-StorageWriter/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"github.com/skynetservices/skynet"
+	"github.com/skynetservices/skynet/client"
 	"github.com/skynetservices/skynet/service"
 	"os"
 )
 
 var (
+	c         *client.Client
 	mongoDb   = skynet.GetDefaultEnvVar("SERVICE_MONGO_DB", "Coverage")
 	mongoHost = skynet.GetDefaultEnvVar("SERVICE_MONGO_HOST", "localhost")
 )
 
 func main() {
+	StartClient()
+	StartService()
+}
+
+func StartClient() {
+	config, _ := skynet.GetClientConfig()
+	c = client.NewClient(config)
+}
+
+func StartService() {
 	config, _ := skynet.GetServiceConfig()
 	config.Name = "StorageWriter"
 	config.Version = "1"
