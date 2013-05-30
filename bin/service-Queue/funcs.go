d60c02f2638387c192d7e60d4cb7d0db6a12d504
jake <bigjake2s@gmail.com>
2013-05-30 15:47:35 -0400

    Set up Queue.AddFeed to read oldest feed from StorageReader

--- bin/service-Queue/funcs.go
+++ bin/service-Queue/funcs.go
@@ -6,13 +6,18 @@ import (
 	"labix.org/v2/mgo/bson"
 )
 
-func (s *Queue) AddFeed(ri *skynet.RequestInfo, in *bson.ObjectId, out interface{}) (err error) {
+func (s *Queue) AddFeed(ri *skynet.RequestInfo, in *interface{}, out *bson.ObjectId) (err error) {
+	ids, err := s.FeedQ.Get()
+	if err != nil {
+		return
+	}
 	feed := &coverage.Feed{}
-	if err = c.GetService("StorageReader", "", "", "").Send(ri, "OldestFeed", in, feed); err != nil {
+	if err = c.GetService("StorageReader", "", "", "").Send(ri, "OldestFeed", ids, feed); err != nil {
 		return
 	}
 	if err = s.FeedQ.Push(feed.ID); err != nil {
 		return
 	}
+	out = &feed.ID
 	return
 }
