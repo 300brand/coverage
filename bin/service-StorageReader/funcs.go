1a4ee22c52693365e7f33b63891490087cc933d9
jake <bigjake2s@gmail.com>
2013-05-30 14:19:52 -0400

    Added GetOldestFeed to StorageReader service

--- bin/service-StorageReader/funcs.go
+++ bin/service-StorageReader/funcs.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"git.300brand.com/coverage"
+	"git.300brand.com/coverage/storage/mongo"
+	"github.com/skynetservices/skynet"
+	"labix.org/v2/mgo/bson"
+)
+
+func (s *StorageReader) OldestFeed(ri *skynet.RequestInfo, in []bson.ObjectId, out *coverage.Feed) (err error) {
+	out, err = s.Mongo.GetOldestFeed(in)
+	return
+}
