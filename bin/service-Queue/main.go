c93ea4a94ea91099585bbb4c1a6f573772637f63
jake <bigjake2s@gmail.com>
2013-05-30 14:11:52 -0400

    Adding foundation for Queue service

--- bin/service-Queue/main.go
+++ bin/service-Queue/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"git.300brand.com/coverage/doozer/idqueue"
+	"github.com/skynetservices/skynet"
+	"github.com/skynetservices/skynet/client"
+	"github.com/skynetservices/skynet/service"
+	"os"
+)
+
+var c *client.ServiceClient
+
+func main() {
+	StartClient()
+	StartService()
+}
+
+func StartClient() {
+	config, _ := skynet.GetClientConfig()
+	c = client.NewClient(config)
+}
+
+func StartService() {
+	config, _ := skynet.GetServiceConfig()
+	config.Name = "Queue"
+	config.Version = "1"
+
+	s := &Queue{
+		Log: skynet.NewConsoleSemanticLogger(config.Name, os.Stdout),
+		FeedQ: &idqueue.IdQueue{
+			Name: "feeds",
+			Addr: config.DoozerConfig.BootUri,
+		},
+	}
+
+	service := service.CreateService(s, config)
+	defer service.Shutdown()
+
+	waiter := service.Start(true)
+	waiter.Wait()
+}
--- bin/service-Queue/service.go
+++ bin/service-Queue/service.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"git.300brand.com/coverage/doozer/idqueue"
 	"github.com/skynetservices/skynet"
 	"github.com/skynetservices/skynet/service"
 )
 
 type Queue struct {
-	Log skynet.SemanticLogger
+	Log   skynet.SemanticLogger
+	FeedQ *idqueue.IdQueue
 }
 
 var _ service.ServiceDelegate = &Queue{}
 
-func (s *Queue) Registered(service *service.Service)   { s.Log.Trace("Registered") }
-func (s *Queue) Unregistered(service *service.Service) { s.Log.Trace("Unregistered") }
-func (s *Queue) Started(service *service.Service)      { s.Log.Trace("Started") }
-func (s *Queue) Stopped(service *service.Service)      { s.Log.Trace("Stopped") }
-func (s *Queue) MethodCalled(method string)            { s.Log.Trace("MethodCalled") }
+func (s *Queue) Registered(service *service.Service) {
+	s.Log.Trace("Registered")
+}
+func (s *Queue) Unregistered(service *service.Service) {
+	s.Log.Trace("Unregistered")
+}
+func (s *Queue) Started(service *service.Service) {
+	s.Log.Trace("Started")
+	if err := s.FeedQ.Connect(); err != nil {
+		panic(err)
+	}
+	s.Log.Trace("Connected to doozer for queue storage")
+}
+func (s *Queue) Stopped(service *service.Service) {
+	s.Log.Trace("Closing connection to doozer for queue storage")
+	s.FeedQ.Close()
+	s.Log.Trace("Stopped")
+}
+func (s *Queue) MethodCalled(method string) {
+	s.Log.Trace("MethodCalled")
+}
 func (s *Queue) MethodCompleted(method string, duration int64, err error) {
 	s.Log.Trace("MethodCompleted")
 }
