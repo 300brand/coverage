1984f2916a7947c8df77b41d7ef5feadc43158d7
jake <bigjake2s@gmail.com>
2013-06-01 06:17:48 -0400

    Added copious amounts of logging in Queue.AddFeed

--- bin/service-Queue/funcs.go
+++ bin/service-Queue/funcs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"git.300brand.com/coverage"
 	"git.300brand.com/coverage/doozer/idqueue"
 	"git.300brand.com/coverage/skytypes"
@@ -8,17 +9,25 @@ import (
 )
 
 func (s *Queue) AddFeed(ri *skynet.RequestInfo, in *skytypes.NullType, out *skytypes.ObjectId) (err error) {
+	s.Log.Debug("Getting current queue")
 	ids, err := s.FeedQ.Get()
 	if err != nil && err != idqueue.ErrEOQ {
+		s.Log.Error(err.Error())
 		return
 	}
+	s.Log.Debug(fmt.Sprintf("Queue retrieved with length of: %d", len(ids)))
 	feed := &coverage.Feed{}
+	s.Log.Debug("Fetching oldest feed")
 	if err = c.GetService("StorageReader", "", "", "").Send(ri, "OldestFeed", ids, feed); err != nil {
+		s.Log.Error(err.Error())
 		return
 	}
+	s.Log.Debug(fmt.Sprintf("Oldest feed ID: %s", feed.ID))
 	if err = s.FeedQ.Push(feed.ID); err != nil {
+		s.Log.Error(err.Error())
 		return
 	}
+	s.Log.Debug("Added feed to queue")
 	out.Id = feed.ID
 	return
 }
