83c0e9592a34a010704995c27ae41adab1257174
jake <bigjake2s@gmail.com>
2013-06-03 16:56:58 -0400

    Moved feed downloading to downloader.Feed; set up feedservice to use

--- downloader/feedservice.go
+++ downloader/feedservice.go
@@ -15,6 +15,10 @@ func NewFeedService() FeedService {
 
 func (s FeedService) Update(f *coverage.Feed) error {
 	f.Log.Service("downloader.FeedService")
+	return Feed(f)
+}
+
+func Feed(f *coverage.Feed) error {
 	r, err := Fetch(f.URL.String())
 	if err != nil {
 		return f.Log.Error(err)
