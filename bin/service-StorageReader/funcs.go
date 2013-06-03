4f966854336a84a07c46fd858d46c1773bc2d14c
jake <bigjake2s@gmail.com>
2013-06-03 10:49:18 -0400

    Fixed transfer issue with dereference

--- bin/service-StorageReader/funcs.go
+++ bin/service-StorageReader/funcs.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"git.300brand.com/coverage"
 	"git.300brand.com/coverage/skytypes"
 	"github.com/skynetservices/skynet"
 )
 
 func (s *StorageReader) OldestFeed(ri *skynet.RequestInfo, in *skytypes.ObjectIds, out *coverage.Feed) (err error) {
-	out, err = s.Mongo.GetOldestFeed(in.Ids)
+	s.Log.Debug(fmt.Sprintf("OldestFeed.in: %+v", in))
+	oldest, err := s.Mongo.GetOldestFeed(in.Ids)
+	*out = *oldest
+	s.Log.Debug(fmt.Sprintf("OldestFeed.out: %+v", out))
 	return
 }
