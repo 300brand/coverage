ab7a06ee066a70b108ecb84265a6ea13d0c85564
jake <bigjake2s@gmail.com>
2013-06-05 12:43:59 -0400

    Added ClockCommand and ClockResult types

--- skytypes/skytypes.go
+++ skytypes/skytypes.go
@@ -2,7 +2,17 @@ package skytypes
 
 import (
 	"labix.org/v2/mgo/bson"
+	"time"
 )
+
+type ClockCommand struct {
+	Command string
+	Tick    time.Duration
+}
+
+type ClockResult struct {
+	Message string
+}
 
 type NullType struct{}
 
