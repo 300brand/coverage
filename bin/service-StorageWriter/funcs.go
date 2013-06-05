7fb32b0876ddbd70da3b121423292f10072cf90e
jake <bigjake2s@gmail.com>
2013-06-05 10:30:01 -0400

    Unified service function internals

--- bin/service-StorageWriter/funcs.go
+++ bin/service-StorageWriter/funcs.go
@@ -6,15 +6,25 @@ import (
 )
 
 func (s *StorageWriter) SaveArticle(ri *skynet.RequestInfo, in *coverage.Article, out *coverage.Article) (err error) {
-	return s.Mongo.UpdateArticle(in)
+	if err = s.Mongo.UpdateArticle(in); err != nil {
+		return
+	}
+	*out = *in
+	return
 }
 
 func (s *StorageWriter) SaveFeed(ri *skynet.RequestInfo, in *coverage.Feed, out *coverage.Feed) (err error) {
+	if err = s.Mongo.UpdateFeed(in); err != nil {
+		return
+	}
 	*out = *in
-	return s.Mongo.UpdateFeed(in)
+	return
 }
 
 func (s *StorageWriter) SavePublication(ri *skynet.RequestInfo, in *coverage.Publication, out *coverage.Publication) (err error) {
+	if err = s.Mongo.UpdatePublication(in); err != nil {
+		return
+	}
 	*out = *in
-	return s.Mongo.UpdatePublication(in)
+	return
 }
