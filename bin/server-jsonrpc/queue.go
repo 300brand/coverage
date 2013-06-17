91cc68de8f37a15eb6fb33bd6b7e833ad149c12d
jake <bigjake2s@gmail.com>
2013-06-17 11:50:53 -0400

    Added Queue.AddOneFeed to JSONRPC server

--- bin/server-jsonrpc/queue.go
+++ bin/server-jsonrpc/queue.go
@@ -16,6 +16,13 @@ func (q *Queue) AddFeed(r *http.Request, in *skytypes.NullType, out *skytypes.Ob
 	return GetService("Queue").Send(nil, "AddFeed", in, out)
 }
 
+func (q *Queue) AddOneFeed(r *http.Request, in *skytypes.NullType, out *skytypes.NullType) (err error) {
+	cmd := &skytypes.ClockCommand{
+		Command: "once",
+	}
+	return GetService("Manager").Send(nil, "QueueFeedAdder", cmd, skytypes.Null)
+}
+
 func (q *Queue) NextFeed(r *http.Request, in *skytypes.NullType, out *coverage.Feed) (err error) {
 	return GetService("Queue").Send(nil, "NextFeed", in, out)
 }
