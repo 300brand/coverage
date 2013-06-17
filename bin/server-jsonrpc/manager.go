1c7e05a63e7a1f50a6ad9ee2bdd37c2be75ceb2d
jake <bigjake2s@gmail.com>
2013-06-17 17:39:46 -0400

    Added call for processing the next feed in the queue

--- bin/server-jsonrpc/manager.go
+++ bin/server-jsonrpc/manager.go
@@ -3,6 +3,7 @@ package main
 import (
 	"git.300brand.com/coverage/skytypes"
 	"net/http"
+	"time"
 )
 
 type Manager struct{}
@@ -18,6 +19,11 @@ func (m *Manager) AddOneFeed(r *http.Request, in *skytypes.NullType, out *skytyp
 	return GetService("Manager").Send(nil, "QueueFeedAdder", cmd, skytypes.Null)
 }
 
-func (m *Manager) ProcessNextFeed(r *http.Request, in *skytypes.ClockCommand, out *skytypes.ClockResult) (err error) {
-	return
+func (m *Manager) ProcessNextFeed(r *http.Request, in *skytypes.ClockCommand, out *skytypes.NullType) (err error) {
+	svc := GetService("Manager")
+	svc.SetTimeout(0, 5*time.Minute)
+	cmd := &skytypes.ClockCommand{
+		Command: "once",
+	}
+	return svc.Send(nil, "FeedProcessor", cmd, skytypes.Null)
 }
