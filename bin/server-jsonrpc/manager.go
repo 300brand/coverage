d44afc2833983b6a017c7d150f159b42f34d64a3
jake <bigjake2s@gmail.com>
2013-06-20 15:43:56 -0400

    Added StartQueue and StopQueue funcs

--- bin/server-jsonrpc/manager.go
+++ bin/server-jsonrpc/manager.go
@@ -19,11 +19,26 @@ func (m *Manager) AddOneFeed(r *http.Request, in *skytypes.NullType, out *skytyp
 	return GetService("Manager").Send(nil, "QueueFeedAdder", cmd, skytypes.Null)
 }
 
-func (m *Manager) ProcessNextFeed(r *http.Request, in *skytypes.ClockCommand, out *skytypes.NullType) (err error) {
+func (m *Manager) ProcessNextFeed(r *http.Request, in *skytypes.NullType, out *skytypes.NullType) (err error) {
 	svc := GetService("Manager")
 	svc.SetTimeout(0, 5*time.Minute)
 	cmd := &skytypes.ClockCommand{
 		Command: "once",
 	}
 	return svc.Send(nil, "FeedProcessor", cmd, skytypes.Null)
+}
+
+func (m *Manager) StartQueue(r *http.Request, in *skytypes.NullType, out *skytypes.ClockCommand) (err error) {
+	out = &skytypes.ClockCommand{
+		Command: "start",
+		Tick:    time.Second * 10,
+	}
+	return GetService("Manager").Send(nil, "FeedProcessor", out, skytypes.Null)
+}
+
+func (m *Manager) StopQueue(r *http.Request, in *skytypes.NullType, out *skytypes.NullType) (err error) {
+	cmd := &skytypes.ClockCommand{
+		Command: "stop",
+	}
+	return GetService("Manager").Send(nil, "FeedProcessor", cmd, skytypes.Null)
 }
