5d47e42826998730cd84c944a60c520b65d986fc
jake <bigjake2s@gmail.com>
2013-06-21 14:45:22 -0400

    Added Manager.{Start,Stop}Feeds; Fixed service calls

--- bin/server-jsonrpc/manager.go
+++ bin/server-jsonrpc/manager.go
@@ -33,10 +33,25 @@ func (m *Manager) StartQueue(r *http.Request, in *skytypes.NullType, out *skytyp
 		Command: "start",
 		Tick:    time.Second * 10,
 	}
-	return GetService("Manager").Send(nil, "FeedProcessor", out, skytypes.Null)
+	return GetService("Manager").Send(nil, "QueueFeedAdder", out, skytypes.Null)
 }
 
 func (m *Manager) StopQueue(r *http.Request, in *skytypes.NullType, out *skytypes.NullType) (err error) {
+	cmd := &skytypes.ClockCommand{
+		Command: "stop",
+	}
+	return GetService("Manager").Send(nil, "QueueFeedAdder", cmd, skytypes.Null)
+}
+
+func (m *Manager) StartFeeds(r *http.Request, in *skytypes.NullType, out *skytypes.ClockCommand) (err error) {
+	out = &skytypes.ClockCommand{
+		Command: "start",
+		Tick:    time.Second * 10,
+	}
+	return GetService("Manager").Send(nil, "FeedProcessor", out, skytypes.Null)
+}
+
+func (m *Manager) StopFeeds(r *http.Request, in *skytypes.NullType, out *skytypes.NullType) (err error) {
 	cmd := &skytypes.ClockCommand{
 		Command: "stop",
 	}
