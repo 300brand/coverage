11a5a2f7e42cb2af85a6a54993a84df7ee928691
jake <bigjake2s@gmail.com>
2013-06-21 15:54:10 -0400

    De-derping Start* calls

--- bin/server-jsonrpc/manager.go
+++ bin/server-jsonrpc/manager.go
@@ -29,10 +29,8 @@ func (m *Manager) ProcessNextFeed(r *http.Request, in *skytypes.NullType, out *s
 }
 
 func (m *Manager) StartQueue(r *http.Request, in *skytypes.NullType, out *skytypes.ClockCommand) (err error) {
-	out = &skytypes.ClockCommand{
-		Command: "start",
-		Tick:    time.Second * 10,
-	}
+	out.Command = "start"
+	out.Tick = time.Second * 10
 	return GetService("Manager").Send(nil, "QueueFeedAdder", out, skytypes.Null)
 }
 
@@ -44,10 +42,8 @@ func (m *Manager) StopQueue(r *http.Request, in *skytypes.NullType, out *skytype
 }
 
 func (m *Manager) StartFeeds(r *http.Request, in *skytypes.NullType, out *skytypes.ClockCommand) (err error) {
-	out = &skytypes.ClockCommand{
-		Command: "start",
-		Tick:    time.Second * 10,
-	}
+	out.Command = "start"
+	out.Tick = time.Second * 10
 	return GetService("Manager").Send(nil, "FeedProcessor", out, skytypes.Null)
 }
 
