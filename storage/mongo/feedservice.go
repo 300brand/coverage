fb96f5f071ae2084e608a2187a804f183c21f157
jake <bigjake2s@gmail.com>
2013-06-24 13:42:25 -0400

    Emptied Feed.Articles before storing.

--- storage/mongo/feedservice.go
+++ storage/mongo/feedservice.go
@@ -87,10 +87,11 @@ func (m *Mongo) GetOldestFeed(ignore []bson.ObjectId) (f *coverage.Feed, err err
 }
 
 func (m *Mongo) UpdateFeed(f *coverage.Feed) (err error) {
+	l := len(f.Articles)
+	f.Articles = f.Articles[:0]
+	defer func(l int) { f.Articles = f.Articles[:l] }(l)
+
 	f.Updated = time.Now()
 	_, err = m.db.C(FeedCollection).UpsertId(f.ID, f)
-	if err != nil {
-		return
-	}
 	return
 }
