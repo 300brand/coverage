2b2261b18109c7d9d375f28c585f1bfc2be005d0
jake <bigjake2s@gmail.com>
2013-06-26 16:40:24 -0400

    Fixed remaining struct paths for Article.Text

--- article_test.go
+++ article_test.go
@@ -14,11 +14,11 @@ func TestChangelog(t *testing.T) {
 	b.Text.Body = Body{
 		Text: []byte("Body text"),
 	}
-	b.Modified("Body")
+	b.Modified("Text")
 
 	merger.Merge(a, b)
 
 	if string(a.Text.Body.Text) != string(b.Text.Body.Text) {
-		t.Error("Body struct did not merge")
+		t.Error("Text struct did not merge")
 	}
 }
