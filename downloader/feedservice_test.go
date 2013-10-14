4adb4dd7902d8835c0f780b398b9c953bf8a1b0a
jake <bigjake2s@gmail.com>
2013-10-14 16:29:28 -0400

    Added downloader test to make sure LastDownload gets updated

--- downloader/feedservice_test.go
+++ downloader/feedservice_test.go
@@ -17,3 +17,14 @@ func TestFeedService(t *testing.T) {
 		t.Errorf("URL not downloaded properly - %d bytes", len(f.Content))
 	}
 }
+
+func TestFeedDownloaded(t *testing.T) {
+	f := coverage.NewFeed()
+	t.Logf("Before: %s", f.LastDownload)
+	cwd, _ := os.Getwd()
+	f.URL, _ = url.Parse("file://" + cwd + "/sample-download.txt")
+	if err := Feed(f); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("After: %s", f.LastDownload)
+}
