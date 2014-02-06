5f1e7a36028fd6339b713cc1da0bdff8074d24a7
jake <bigjake2s@gmail.com>
2014-02-06 18:01:10 -0500

    Added Pages to store additional pages for an article in the Text struct

--- text.go
+++ text.go
@@ -2,6 +2,7 @@ package coverage
 
 type Text struct {
 	Words Words
-	HTML  []byte
+	HTML  []byte   // HTML for article (Page 1)
+	Pages [][]byte // HTML for additional pages (i:0 == Page 2)
 	Body  Body
 }
