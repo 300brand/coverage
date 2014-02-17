f5f46bb4109c71ad03e080e551bd9576615831dd
jake <bigjake2s@gmail.com>
2014-02-17 18:11:53 -0500

    Built in multipage article body extraction and composition with test

--- article/body/articleservice.go
+++ article/body/articleservice.go
@@ -2,6 +2,7 @@ package body
 
 import (
 	"errors"
+	"fmt"
 	"github.com/300brand/coverage"
 	"github.com/300brand/coverage/service"
 	"github.com/300brand/logger"
@@ -27,14 +28,30 @@ func SetBody(a *coverage.Article) (err error) {
 		logger.Error.Printf("SetBody: A %s %s", a.ID.Hex(), err)
 		return
 	}
-	cleaned, err := CleanHTML(a.Text.HTML)
-	if err != nil {
-		logger.Error.Printf("SetBody: A %s %s", a.ID.Hex(), err)
-		return
-	}
-	if a.Text.Body, err = GetBody(cleaned); err != nil {
-		logger.Error.Printf("SetBody: A %s %s", a.ID.Hex(), err)
-		return
+
+	var cleaned []byte
+	var body coverage.Body
+	pages := make([][]byte, 1, len(a.Text.Pages)+1)
+	pages[0] = a.Text.HTML
+	pages = append(pages, a.Text.Pages...)
+
+	for i, html := range pages {
+		cleaned, err = CleanHTML(html)
+		if err != nil {
+			logger.Error.Printf("SetBody: A %s %s", a.ID.Hex(), err)
+			return
+		}
+		if body, err = GetBody(cleaned); err != nil {
+			logger.Error.Printf("SetBody: A %s %s", a.ID.Hex(), err)
+			return
+		}
+
+		a.Text.Body.HTML = append(a.Text.Body.HTML, []byte(fmt.Sprintf(`<div class="page-%d">`, i+1))...)
+		a.Text.Body.HTML = append(a.Text.Body.HTML, body.HTML...)
+		a.Text.Body.HTML = append(a.Text.Body.HTML, []byte(`</div>`)...)
+
+		a.Text.Body.Text = append(a.Text.Body.Text, body.Text...)
+		a.Text.Body.Text = append(a.Text.Body.Text, '\n')
 	}
 	logger.Debug.Printf("SetBody: A %s Updated body", a.ID.Hex())
 	return
